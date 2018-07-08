640a45bf03656ce1ef5846956e726c884cb224cf
Solin_glb <Solin_glb@svip.com>
2018-07-08 22:41:19 +0800

    提交修改

--- src/main/java/com/solin/git/GitHub.java
+++ src/main/java/com/solin/git/GitHub.java
@@ -3,6 +3,6 @@ package com.solin.git;
 public class GitHub {
 	public static void main(String[] args) {
 		System.out.println("GitHub......");
-		System.out.println("ÐÞ¸Ä¡£¡£¡£");
+		System.out.println("ä¿®æ”¹ã€‚ã€‚ã€‚");
 	}
 }
